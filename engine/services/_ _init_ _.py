f71630202e6d43554aa70480b2ec60edae75323c
Codewithmerlin <168102728+Merlin1235@users.noreply.github.com>
2026-09-12 13:29:54 +0300

    Import services and define __all__ in __init__.py

--- engine/services/_ _init_ _.py
+++ engine/services/_ _init_ _.py
@@ -1,1 +1,11 @@
+from .database import Database
+from .detector import ServiceDetector, ServiceInfo
+from .repository import AssessmentRepository
+
+__all__ = [
+    "AssessmentRepository",
+    "Database",
+    "ServiceDetector",
+    "ServiceInfo",
+]
 
